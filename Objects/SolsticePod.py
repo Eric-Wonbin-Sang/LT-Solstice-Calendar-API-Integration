ff7dbadb02cb30dfc259e0ef37bb494a319f334f
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-05-18 11:43:07 -0400

    Added the is_valid_ip function that checks if the ip is valid.

--- Objects/SolsticePod.py
+++ Objects/SolsticePod.py
@@ -10,16 +10,20 @@ class SolsticePod:
 
         self.ip_address = kwargs.get("ip_address")
         self.calendar_id = kwargs.get("calendar_id")
+        self.service = kwargs.get("service")
 
-    def get_event_list(self, service):
-        events_result = service.events().list(calendarId=self.calendar_id,
-                                              timeMin=datetime.datetime.utcnow().isoformat() + 'Z',  # 'Z' for UTC time
-                                              maxResults=10,
-                                              singleEvents=True,
-                                              orderBy='startTime').execute()
+        self.calendar_item_list = self.get_calendar_item_list()
+
+    def get_event_list(self):
+        events_result = self.service.events().list(calendarId=self.calendar_id,
+                                                   timeMin=datetime.datetime.utcnow().isoformat() + 'Z',
+                                                   # 'Z' for UTC time
+                                                   maxResults=10,
+                                                   singleEvents=True,
+                                                   orderBy='startTime').execute()
         return events_result.get('items', [])
 
-    def get_calendar_item_list(self, service):
+    def get_calendar_item_list(self):
 
         curr_date = datetime.datetime.now()
         now = curr_date.isoformat() + 'Z'
@@ -28,7 +32,7 @@ class SolsticePod:
         now_local = time.localtime(epoch_now)
 
         # Get future event info using calendarID
-        event_list = self.get_event_list(service=service)
+        event_list = self.get_event_list()
         if not event_list:
             print('No upcoming events found.')
         calendar_item_list = []
@@ -42,12 +46,19 @@ class SolsticePod:
 
         return calendar_item_list
 
+    def get_calendar_dict_list(self):
+        return [calendar_item.get_solstice_calendar_dict(calendar_item_index=e_i)
+                for e_i, calendar_item in enumerate(self.calendar_item_list)]
 
-def get_solstice_pod_list(calendar_info):
+
+def get_solstice_pod_list(calendar_info, service):
     solstice_pod_list = []
     for ip_address in calendar_info.ip_id_dict:
-        solstice_pod_list.append(SolsticePod(
-            ip_address=ip_address.strip(),
-            calendar_id=calendar_info.ip_id_dict[ip_address])
+        solstice_pod_list.append(
+            SolsticePod(
+                ip_address=ip_address.strip(),
+                calendar_id=calendar_info.ip_id_dict[ip_address],
+                service=service
+            )
         )
     return solstice_pod_list
--- main.py
+++ main.py
@@ -9,20 +9,33 @@ from Objects import SolsticePod, CalendarInfo
 from General import Constants
 
 
+def is_valid_ip(calendar_info, ip_address):
+    admin_password = calendar_info.param_dict["Solstice Password"]
+    stats_url = 'http://{}/api/stats?password={}'.format(ip_address, admin_password)
+    try:
+        rc = requests.get(stats_url)
+        return True
+    except:
+        return False
+
+
 def get_password(calendar_info, ip_address):
+
+    admin_password = calendar_info.param_dict["Solstice Password"]
+
     # Return empty string if clear-text password is empty
-    if calendar_info.param_dict["Solstice Password"] is None:
+    if admin_password is None:
         return ''
 
-    stats_url = 'http://{}/api/stats?password={}'.format(ip_address, calendar_info.param_dict["Solstice Password"])
+    stats_url = 'http://{}/api/stats?password={}'.format(ip_address, admin_password)
     rc = requests.get(stats_url)
     request_config = eval(rc.text)
     version = request_config.get('m_serverVersion')
 
     # Return a SHA1-hashed password string if Pod version < 4.1
     if float(version[0:3]) < 4.1:
-        return hashlib.sha1(calendar_info.param_dict["Solstice Password"].encode('utf-8')).hexdigest()
-    return calendar_info.param_dict["Solstice Password"]
+        return hashlib.sha1(admin_password.encode('utf-8')).hexdigest()
+    return admin_password
 
 
 def get_credentials():
@@ -44,22 +57,21 @@ def main():
     credentials = get_credentials()
     service = build('calendar', 'v3', http=credentials.authorize(Http()))
 
-    for solstice_pod in SolsticePod.get_solstice_pod_list(calendar_info=calendar_info):
+    for solstice_pod in SolsticePod.get_solstice_pod_list(calendar_info=calendar_info, service=service):
 
-        calendar_item_list = solstice_pod.get_calendar_item_list(service=service)
-        for i, calendar_item in enumerate(calendar_item_list):
-            print(i, calendar_item)
+        if is_valid_ip(calendar_info, solstice_pod.ip_address):
+            requests.post(
+                'http://{}/api/calendar/set'.format(solstice_pod.ip_address),
+                json={
+                    'password': get_password(calendar_info, solstice_pod.ip_address),
+                    'calendarItems': solstice_pod.get_calendar_dict_list()
+                }
+            )
 
-        calendar_dict_list = [calendar_item.get_solstice_calendar_dict(calendar_item_index=e_i)
-                              for e_i, calendar_item in enumerate(calendar_item_list)]
-
-        requests.post(
-            'http://{}/api/calendar/set'.format(solstice_pod.ip_address),
-            json={
-                'password': get_password(calendar_info, solstice_pod.ip_address),
-                'calendarItems': calendar_dict_list
-            }
-        )
+            for i, calendar_item in enumerate(solstice_pod.calendar_item_list):
+                print(i, calendar_item)
+        else:
+            print(solstice_pod.ip_address, "is not valid")
 
 
 main()
